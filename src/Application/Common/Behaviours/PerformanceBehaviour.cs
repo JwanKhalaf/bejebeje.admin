4fc3cd0a57afcedc8d3affadab0d5c47280afb93
Jwan Khalaf <42877376+JwanKhalaf@users.noreply.github.com>
2022-12-31 01:29:57 +0000

    update project to .net 7.0 (#14)

--- src/Application/Common/Behaviours/PerformanceBehaviour.cs
+++ src/Application/Common/Behaviours/PerformanceBehaviour.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Logging;
 
 namespace bejebeje.admin.Application.Common.Behaviours;
 
-public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
     private readonly Stopwatch _timer;
     private readonly ILogger<TRequest> _logger;
@@ -21,7 +21,7 @@ public class PerformanceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequ
         _currentUserService = currentUserService;
     }
 
-    public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         _timer.Start();
 
