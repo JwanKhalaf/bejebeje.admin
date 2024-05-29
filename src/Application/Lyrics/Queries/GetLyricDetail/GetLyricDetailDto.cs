018e278f98b3350fd43e54405c2ee13afbda7934
Jwan Khalaf <jwan.khalaf@outlook.com>
2024-05-29 23:34:22 +0100

    add ability to approve and unapprove a lyric

--- src/Application/Lyrics/Queries/GetLyricDetail/GetLyricDetailDto.cs
+++ src/Application/Lyrics/Queries/GetLyricDetail/GetLyricDetailDto.cs
@@ -19,6 +19,8 @@ public class GetLyricDetailDto : IMapFrom<Lyric>
 
     public bool IsVerified { get; set; }
 
+    public bool IsApproved { get; set; }
+
     public ArtistDto Artist { get; set; }
 }
 
