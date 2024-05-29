8b038d8f3c209acd0d96d903381007425e18f53b
J Khalaf <42877376+JwanKhalaf@users.noreply.github.com>
2024-05-29 23:34:03 +0100

    add ability to approve and unapprove a lyric (#38)

---
+++ src/Application/Lyrics/Commands/ApproveLyric/ApproveLyricCommand.cs
@@ -0,0 +1,40 @@
+﻿using bejebeje.admin.Application.Common.Interfaces;
+using bejebeje.admin.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace bejebeje.admin.Application.Lyrics.Commands.ApproveLyric;
+
+public class ApproveLyricCommand : IRequest
+{
+    public int LyricId { get; set; }
+}
+
+public class ApproveLyricCommandHandler : IRequestHandler<ApproveLyricCommand>
+{
+    private readonly IDateTime _dateTime;
+    private readonly IApplicationDbContext _context;
+
+    public ApproveLyricCommandHandler(
+        IDateTime dateTime,
+        IApplicationDbContext context)
+    {
+        _dateTime = dateTime;
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(ApproveLyricCommand command, CancellationToken cancellationToken)
+    {
+        Lyric entity = await _context
+            .Lyrics
+            .Where(l => l.Id == command.LyricId)
+            .SingleAsync(cancellationToken);
+
+        entity.IsApproved = true;
+        entity.ModifiedAt = _dateTime.Now;
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
---
+++ src/Application/Lyrics/Commands/UnapproveLyric/UnapproveLyricCommand.cs
@@ -0,0 +1,40 @@
+﻿using bejebeje.admin.Application.Common.Interfaces;
+using bejebeje.admin.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace bejebeje.admin.Application.Lyrics.Commands.UnapproveLyric;
+
+public class UnapproveLyricCommand : IRequest
+{
+    public int LyricId { get; set; }
+}
+
+public class UnapproveLyricCommandHandler : IRequestHandler<UnapproveLyricCommand>
+{
+    private readonly IDateTime _dateTime;
+    private readonly IApplicationDbContext _context;
+
+    public UnapproveLyricCommandHandler(
+        IDateTime dateTime,
+        IApplicationDbContext context)
+    {
+        _dateTime = dateTime;
+        _context = context;
+    }
+
+    public async Task<Unit> Handle(UnapproveLyricCommand command, CancellationToken cancellationToken)
+    {
+        Lyric entity = await _context
+            .Lyrics
+            .Where(l => l.Id == command.LyricId)
+            .SingleAsync(cancellationToken);
+
+        entity.IsApproved = false;
+        entity.ModifiedAt = _dateTime.Now;
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
